a9343f91a402995a7e628a35b33a65c91854ee6e
梁艳萍 <1240089046@qq.com>
2019-06-03 10:47:16 +0800

    我的博客

---
+++ blog/admin.py
@@ -0,0 +1,48 @@
+from django.contrib import admin
+from .models import Banner, Category, Tag, Tui, Article, Link
+#导入需要管理的数据库表
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'category', 'title', 'tui', 'user', 'views', 'created_time')
+    list_fliter = ('id', 'category', 'title', 'tui', 'user', 'views', 'created_time')
+    search_fields = ('id', 'category', 'title', 'tui', 'user', 'views')
+    # 文章列表里显示想要显示的字段
+    list_per_page = 50
+    # 满50条数据就自动分页
+    ordering = ('-created_time',)
+    #后台数据列表排序方式
+    list_display_links = ('id', 'title')
+    # 设置哪些字段可以点击进入编辑界面
+
+
+
+@admin.register(Banner)
+class BannerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'text_info', 'img', 'link_url', 'is_active')
+    list_fliter = ('id', 'text_info', 'img', 'link_url', 'is_active')
+    search_fields = ('id', 'text_info', 'img', 'link_url', 'is_active')
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'index')
+    list_fliter = ('id', 'name', 'index')
+    search_fields = ('id', 'name', 'index')
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_fliter = ('id', 'name')
+    search_fields = ('id', 'name')
+
+@admin.register(Tui)
+class TuiAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_fliter = ('id', 'name')
+    search_fields = ('id', 'name')
+
+@admin.register(Link)
+class LinkAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name','linkurl')
+    list_fliter = ('id', 'name', 'linkurl')
+    search_fields= ('id', 'name', 'linkurl')
